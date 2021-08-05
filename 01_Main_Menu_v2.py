f9140a2a5db0acd4d2796339b85b75e54e778d29
Wardl1 <66809651+Wardl1@users.noreply.github.com>
2021-08-05 14:25:53 +1200

    Create 01_Main_Menu_v2.py

---
+++ 01_Main_Menu_v2.py
@@ -0,0 +1,52 @@
+from tkinter import *
+
+
+class Math:
+    def __init__(self):
+        # Formatting variables
+        background_color = "#66FFFF"  # light blue
+
+        # Main menu GUI frame
+        self.main_menu_frame = Frame(width=300, height=300,
+                                     bg=background_color, pady=10)
+        self.main_menu_frame.grid()
+
+        # Math Quiz heading (row 0)
+        self.math_quiz_label = Label(self.main_menu_frame,
+                                     text="Math Quiz",
+                                     font=("Arial", "16", "bold"),
+                                     bg=background_color,
+                                     padx=10, pady=10)
+        self.math_quiz_label.grid(row=0)
+
+        # Simple instructions given
+        self.intstruction_label = Label(self.main_menu_frame,
+                                        text="Pick one area of math"
+                                             " to work on \n and answer "
+                                             "the 10 questions given.",
+                                        font=("Arial", "12", "italic"),
+                                        bg=background_color,
+                                        padx=10, pady=10)
+        self.intstruction_label.grid(row=1)
+
+        # Addition button (row 2)
+        self.addition_button = Button(self.main_menu_frame, text="Addition",
+                                      font=("Arial", "14"),
+                                      padx=10, pady=10,
+                                      bg="#008CFF",  # darker blue
+                                      fg="white",
+                                      command=self.math_addition())
+        self.addition_button.grid(row=2)
+
+    def math_addition(self):
+        print("1 + 1 = ")
+        # get_addition = Addition()
+        # get_addition.help_text.configure(text="1 + 1 = ")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Math Quiz")
+    something = Math()
+    root.mainloop()
